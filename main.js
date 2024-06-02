5d6896638e5b0f4ff369d6e71e269709daae4189
samuqeet123 <sabdulmuqeetburki@gmail.com>
2024-06-02 14:59:23 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,74 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    {
+        type: "list",
+        name: "Question1",
+        message: "Which one is the biggest city of Pakistan?",
+        choices: ["Karachi", "Islamabad", "Non of them"]
+    },
+    {
+        type: "list",
+        name: "Question2",
+        message: "Who is the current Prime Minister of Pakistan?",
+        choices: ["Imran Khan", "Shahbaz Sharif", "Asim Munir"]
+    },
+    {
+        type: "list",
+        name: "Question3",
+        message: "What is the formula of water?",
+        choices: ["H2O", "CO2", "Non of Them"]
+    },
+    {
+        type: "list",
+        name: "Question4",
+        message: "Which one is the fruit from the following",
+        choices: ["Tomato", "Potato", "Non of Them"]
+    }
+]);
+let Q1 = answer.Question1;
+let score = 0;
+if (Q1 == "Karachi") {
+    score += 1;
+}
+else {
+    score;
+}
+let Q2 = answer.Question2;
+if (Q2 == "Asim Munir") {
+    score += 1;
+}
+else {
+    score;
+}
+let Q3 = answer.Question3;
+if (Q3 == "H2O") {
+    score += 1;
+}
+else {
+    score;
+}
+let Q4 = answer.Question4;
+if (Q4 == "Tomato") {
+    score += 1;
+}
+else {
+    score;
+}
+switch (score) {
+    case 4:
+        console.log("Excellent");
+        break;
+    case 3:
+        console.log("Very Good");
+        break;
+    case 2:
+        console.log("Keep it up");
+        break;
+    case 1:
+        console.log("Nice try");
+        break;
+    default:
+        console.log("letter luck next time");
+        break;
+}
